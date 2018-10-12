2b0ffc14e1b7d9a2237affe7545d19d15e11d426
Roni Peled <roni@mediajel.com>
2018-10-12 19:29:52 -0400

    added scoping

--- drivers/src/chassis/physical.rs
+++ drivers/src/chassis/physical.rs
@@ -4,7 +4,8 @@
 */
 extern crate rppal; 
 
-use rppal::gpio::{Gpio, High, Low, Output};
+use rppal::gpio::{Gpio, Output, Level};
+use Level::{High, Low};
 
 const ENABLE_A: u32 = 1;
 const ENABLE_B: u32 = 2;
