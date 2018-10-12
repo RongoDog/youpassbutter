ba0f93e4bcbe58cb3f0805ef6ee375177ae7e019
Roni Peled <roni@mediajel.com>
2018-10-12 19:34:43 -0400

    Fixed scoping

--- drivers/src/chassis/physical.rs
+++ drivers/src/chassis/physical.rs
@@ -5,8 +5,6 @@
 extern crate rppal; 
 
 use rppal::gpio;
-use rppal::gpio::{Gpio, Output, Level};
-use rppal::Level::{High, Low};
 
 const ENABLE_A: u32 = 1;
 const ENABLE_B: u32 = 2;
@@ -16,45 +14,45 @@ const IN_3: u32 = 5;
 const IN_4: u32 = 6;
 
 // Some templates
-fn stop(gpio: Gpio) -> bool {
-    gpio.write(ENABLE_A, Low);
-    gpio.write(ENABLE_B, Low);
-    gpio.write(IN_1, Low);
-    gpio.write(IN_2, Low);
-    gpio.write(IN_3, Low);
-    gpio.write(IN_4, Low);
+fn stop(gpio: gpio::Gpio) -> bool {
+    gpio.write(ENABLE_A, gpio::Level::Low);
+    gpio.write(ENABLE_B, gpio::Level::Low);
+    gpio.write(IN_1, gpio::Level::Low);
+    gpio.write(IN_2, gpio::Level::Low);
+    gpio.write(IN_3, gpio::Level::Low);
+    gpio.write(IN_4, gpio::Level::Low);
     return true;
 }
 
-fn forward(gpio: Gpio) -> bool {
-    gpio.write(ENABLE_A, High);
-    gpio.write(ENABLE_B, High);
-    gpio.write(IN_2, Low);
-    gpio.write(IN_1, High);
-    gpio.write(IN_4, Low);
-    gpio.write(IN_3, High);
+fn forward(gpio: gpio::Gpio) -> bool {
+    gpio.write(ENABLE_A, gpio::Level::High);
+    gpio.write(ENABLE_B, gpio::Level::High);
+    gpio.write(IN_2, gpio::Level::Low);
+    gpio.write(IN_1, gpio::Level::High);
+    gpio.write(IN_4, gpio::Level::Low);
+    gpio.write(IN_3, gpio::Level::High);
     return true;
 }
 
-fn backward(gpio: Gpio) -> bool {
-    gpio.write(ENABLE_A, High);
-    gpio.write(ENABLE_B, High);
-    gpio.write(IN_1, Low);
-    gpio.write(IN_2, High);
-    gpio.write(IN_3, Low);
-    gpio.write(IN_4, High);
+fn backward(gpio: gpio::Gpio) -> bool {
+    gpio.write(ENABLE_A, gpio::Level::High);
+    gpio.write(ENABLE_B, gpio::Level::High);
+    gpio.write(IN_1, gpio::Level::Low);
+    gpio.write(IN_2, gpio::Level::High);
+    gpio.write(IN_3, gpio::Level::Low);
+    gpio.write(IN_4, gpio::Level::High);
     return true;
 }
 
 // This function should initialize the GPIO
 // NOTE THAT THIS SHOULD BE MOVED ONCES WE ADD PERIPHERALS
 fn initialize(gpio: gpio::Gpio) -> bool {
-    gpio.set_mode(ENABLE_A, gpio::Output);
-    gpio.set_mode(ENABLE_B, gpio::Output);
-    gpio.set_mode(IN_1, gpio::Output);
-    gpio.set_mode(IN_2, gpio::Output);
-    gpio.set_mode(IN_3, gpio::Output);
-    gpio.set_mode(IN_4, gpio::Output);
+    gpio.set_mode(ENABLE_A, gpio::Mode::Output);
+    gpio.set_mode(ENABLE_B, gpio::Mode::Output);
+    gpio.set_mode(IN_1, gpio::Mode::Output);
+    gpio.set_mode(IN_2, gpio::Mode::Output);
+    gpio.set_mode(IN_3, gpio::Mode::Output);
+    gpio.set_mode(IN_4, gpio::Mode::Output);
     stop(gpio);
     return true;
 }
