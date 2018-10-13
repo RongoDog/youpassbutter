48d4038f293e3535b756eef95e2f239d653e4fc4
Roni Peled <roni@mediajel.com>
2018-10-12 20:54:51 -0400

    bugfix

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -5,7 +5,7 @@ extern crate rppal;
 use std::{thread, time};
 
 fn main() {
-    let second = time::Duration::from_millis(1000);
+    let second = time::Duration::from_millis(100);
     let mut gpio: rppal::gpio::Gpio = rppal::gpio::Gpio::new().unwrap();
     drivers::chassis::physical::initialize(&mut gpio);
 
