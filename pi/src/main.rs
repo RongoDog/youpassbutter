0e52a1baa7a1392749d755d30298cfd1675eb3fc
Roni Peled <roni@mediajel.com>
2018-10-12 20:22:34 -0400

    Fixed namespacing

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -15,14 +15,14 @@ fn main() {
             step = 1;
         }
         match step {
-            1 => drivers::chassis::first_step(gpio),
-            2 => drivers::chassis::second_step(gpio),
-            3 => drivers::chassis::third_step(gpio),
-            4 => drivers::chassis::fourth_step(gpio),
-            5 => drivers::chassis::fifth_step(gpio),
-            6 => drivers::chassis::sixth_step(gpio),
-            7 => drivers::chassis::seventh_step(gpio),
-            8 => drivers::chassis::eight_step(gpio)
+            1 => drivers::chassis::physical::first_step(gpio),
+            2 => drivers::chassis::physical::second_step(gpio),
+            3 => drivers::chassis::physical::third_step(gpio),
+            4 => drivers::chassis::physical::fourth_step(gpio),
+            5 => drivers::chassis::physical::fifth_step(gpio),
+            6 => drivers::chassis::physical::sixth_step(gpio),
+            7 => drivers::chassis::physical::seventh_step(gpio),
+            8 => drivers::chassis::physical::eight_step(gpio)
         }
         step ++;
         thread::sleep(ten_millis*100);
