d827fefb5edd909f29d22d9b2252fd72c7143cb7
Roni Peled <roni@mediajel.com>
2018-10-12 20:43:20 -0400

    bugfix

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -7,7 +7,7 @@ use std::{thread, time};
 fn main() {
     let ten_millis = time::Duration::from_millis(10);
     let mut gpio: rppal::gpio::Gpio = rppal::gpio::Gpio::new().unwrap();
-    drivers::chassis::physical::initialize(&mut gpio);
+    drivers::chassis::physical::initialize(gpio);
 
     let mut step = 1;
     while true {
@@ -15,15 +15,15 @@ fn main() {
             step = 1;
         }
         match step {
-            1 => drivers::chassis::physical::first_step(&mut gpio),
-            2 => drivers::chassis::physical::second_step(&mut gpio),
-            3 => drivers::chassis::physical::third_step(&mut gpio),
-            4 => drivers::chassis::physical::fourth_step(&mut gpio),
-            5 => drivers::chassis::physical::fifth_step(&mut gpio),
-            6 => drivers::chassis::physical::sixth_step(&mut gpio),
-            7 => drivers::chassis::physical::seventh_step(&mut gpio),
-            8 => drivers::chassis::physical::eight_step(&mut gpio),
-            _ => drivers::chassis::physical::fifth_step(&mut gpio)
+            1 => drivers::chassis::physical::first_step(gpio),
+            2 => drivers::chassis::physical::second_step(gpio),
+            3 => drivers::chassis::physical::third_step(gpio),
+            4 => drivers::chassis::physical::fourth_step(gpio),
+            5 => drivers::chassis::physical::fifth_step(gpio),
+            6 => drivers::chassis::physical::sixth_step(gpio),
+            7 => drivers::chassis::physical::seventh_step(gpio),
+            8 => drivers::chassis::physical::eight_step(gpio),
+            _ => drivers::chassis::physical::fifth_step(gpio)
         }
         step += 1;
         thread::sleep(ten_millis*100);
