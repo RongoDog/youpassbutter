587871c3fff6411fdd932733052887d6a2394802
Roni Peled <roni@mediajel.com>
2018-11-02 20:25:47 -0400

    Continued main.rs file

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -1,4 +1,7 @@
 extern crate youpassbutter;
+extern crate ws;
+
+use ws::{connect, CloseCode};
 
 fn main() {
     // Wcreate the chassis state machine. 
@@ -9,5 +12,24 @@ fn main() {
   };
   let shared_state: SharedState = SharedState::new(true, current_position);
   let mut chassis: Chassis = Chassis::new(shared_state);
-  
+
+  // Connect to the url and call the closure
+  if let Err(error) = connect("ws://127.0.0.1:3012", |out| {
+      // Queue a message to be sent when the WebSocket is open
+      if out.send("Hello WebSocket").is_err() {
+          println!("Websocket couldn't queue an initial message.")
+      } else {
+          println!("Client sent message 'Hello WebSocket'. ")
+      }
+      // The handler needs to take ownership of out, so we use move
+      move |msg| {
+          // Handle messages received on this connection
+          println!("Client got message '{}'. ", msg);
+          // Close the connection
+          out.close(CloseCode::Normal)
+      }
+  }) {
+      // Inform the user of failure
+      println!("Failed to create WebSocket due to: {:?}", error);
+  }  
 }
