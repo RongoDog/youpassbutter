8966b67c9e5bf3c543038168c3815f7b68c8a685
Roni Peled <roni@mediajel.com>
2018-11-07 19:27:51 -0500

    Removed unnecessary code

--- server/emekas_server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const path = require('path')
-const PORT = process.env.PORT || 8000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
