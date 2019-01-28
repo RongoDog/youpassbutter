727adc291de0554210561c022bf8bc1115b0af23
Roni Peled <roni@mediajel.com>
2019-01-28 15:49:35 -0500

    Include encoding code

--- robot/src/drivers/mpu6050.cpp
+++ robot/src/drivers/mpu6050.cpp
@@ -1,3 +1,88 @@
+/*
+ * Base64 encoding/decoding (RFC1341)
+ * Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
+
+#include "includes.h"
+
+#include "os.h"
+#include "base64.h"
+
+static const unsigned char base64_table[65] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+/**
+ * base64_encode - Base64 encode
+ * @src: Data to be encoded
+ * @len: Length of the data to be encoded
+ * @out_len: Pointer to output length variable, or %NULL if not used
+ * Returns: Allocated buffer of out_len bytes of encoded data,
+ * or %NULL on failure
+ *
+ * Caller is responsible for freeing the returned buffer. Returned buffer is
+ * nul terminated to make it easier to use as a C string. The nul terminator is
+ * not included in out_len.
+ */
+unsigned char *base64_encode(const unsigned char *src, size_t len,
+			      size_t *out_len)
+{
+	unsigned char *out, *pos;
+	const unsigned char *end, *in;
+	size_t olen;
+	int line_len;
+
+	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+	olen += olen / 72; /* line feeds */
+	olen++; /* nul termination */
+	if (olen < len)
+		return NULL; /* integer overflow */
+	out = os_malloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	end = src + len;
+	in = src;
+	pos = out;
+	line_len = 0;
+	while (end - in >= 3) {
+		*pos++ = base64_table[in[0] >> 2];
+		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+		*pos++ = base64_table[in[2] & 0x3f];
+		in += 3;
+		line_len += 4;
+		if (line_len >= 72) {
+			*pos++ = '\n';
+			line_len = 0;
+		}
+	}
+
+	if (end - in) {
+		*pos++ = base64_table[in[0] >> 2];
+		if (end - in == 1) {
+			*pos++ = base64_table[(in[0] & 0x03) << 4];
+			*pos++ = '=';
+		} else {
+			*pos++ = base64_table[((in[0] & 0x03) << 4) |
+					      (in[1] >> 4)];
+			*pos++ = base64_table[(in[1] & 0x0f) << 2];
+		}
+		*pos++ = '=';
+		line_len += 4;
+	}
+
+	if (line_len)
+		*pos++ = '\n';
+
+	*pos = '\0';
+	if (out_len)
+		*out_len = pos - out;
+	return out;
+}
+
 #include "drivers/pinout_definitions.h"
 #include "globals.h"
 
@@ -54,6 +139,7 @@ extern "C" {
 // Amount of data stored in the FIFO read H->L
 #define FIFO_COUNT_H_REG          0x72 //   //R
 #define FIFO_COUNT_L_REG          0x73 //   //R 
+
 
 extern "C" void* initialize_mpu6050(void *arg){
 	struct thread_info *info = (struct thread_info *)arg;
@@ -205,7 +291,7 @@ extern "C" void* initialize_mpu6050(void *arg){
 		// We intend on reading a full dataset at a time. For 12 bytes. As such we perform a modulus operation
 		// to determine any extra bytes. 
 		int nb_reads = count_value - (count_value % 12);
-		void* buffer = malloc(nb_reads*sizeof(char));
+		char buffer[nb_reads]; 
 		int total_read = 0;
 		while (total_read < nb_reads) {
 			int to_read = 32;
@@ -222,11 +308,12 @@ extern "C" void* initialize_mpu6050(void *arg){
 
 		// If we've read some data, we send it over the websocket we assume is open
 		if (total_read > 0) {
-			ssize_t sent = send(info->socketfd, buffer, total_read, MSG_EOR);
+			size_t encoded_length; 
+			unsigned char *encoded = base64_encode(buffer, total_read, encoded_length)
+			ssize_t sent = send(info->socketfd, encoded, encoded_length, MSG_EOR);
 			if (sent < 0) {
 				fprintf(stderr, "Failed to send all necessary MPU6050 data");
 			}
-			free(buffer);
 		}
 		sem_post(i2c_semaphore);
 	}
