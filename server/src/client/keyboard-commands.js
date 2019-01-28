4607fd98956b77929319947ae4040ba3bab113d2
Roni Peled <roni@mediajel.com>
2019-01-28 14:38:34 -0500

    test commit

--- server/src/client/keyboard-commands.js
+++ server/src/client/keyboard-commands.js
@@ -2,38 +2,36 @@ function setupKeyCommands(dataChannel) {
   var bufferArray = new ArrayBuffer(1);
   var byteArray = new Uint8Array(bufferArray);
 
-  window.addEventListener("load", (e) => {
-    document.addEventListener("keypress", (event) => {
-      event.preventDefault();
-      console.log("Received command", event.code);
-      switch (event.code) {
-        case "KeyW":
-          byteArray[0] = 1;
-          dataChannel.send(byteArray);
-          break;
-        case "KeyA":
-          byteArray[0] = 2;
-          dataChannel.send(byteArray);
-          break;
-        case "KeyD":
-          byteArray[0] = 3;
-          dataChannel.send(byteArray);
-          break;
-        case "KeyS":
-          byteArray[0] = 4;
-          dataChannel.send(byteArray);
-          break;
-        case "KeyE":
-          byteArray[0] = 5;
-          dataChannel.send(byteArray);
-          break;
-        case "KeyO":
-          byteArray[0] = 6;
-          dataChannel.send(byteArray);
-          break;
-        default:
-          break;
-      }
-    });
+  document.addEventListener("keypress", (event) => {
+    event.preventDefault();
+    console.log("Received command", event.code);
+    switch (event.code) {
+      case "KeyW":
+        byteArray[0] = 1;
+        dataChannel.send(byteArray);
+        break;
+      case "KeyA":
+        byteArray[0] = 2;
+        dataChannel.send(byteArray);
+        break;
+      case "KeyD":
+        byteArray[0] = 3;
+        dataChannel.send(byteArray);
+        break;
+      case "KeyS":
+        byteArray[0] = 4;
+        dataChannel.send(byteArray);
+        break;
+      case "KeyE":
+        byteArray[0] = 5;
+        dataChannel.send(byteArray);
+        break;
+      case "KeyO":
+        byteArray[0] = 6;
+        dataChannel.send(byteArray);
+        break;
+      default:
+        break;
+    }
   });
 }
