43eded2adcd4900830d4d582f5bcec6c7ca2fc81
Roni Peled <roni@mediajel.com>
2019-01-28 16:31:39 -0500

    print hex

--- server/src/client/data-processing.js
+++ server/src/client/data-processing.js
@@ -1,3 +1,13 @@
 function processIncomingData(data) {
-  console.log("Received", window.atob(data + "/0").toString('hex'));
+  var raw =  window.atob;
+  var HEX = '';
+
+  for ( i = 0; i < raw.length; i++ ) {
+
+    var _hex = raw.charCodeAt(i).toString(16)
+
+    HEX += (_hex.length==2?_hex:'0'+_hex);
+
+  }
+  console.log(HEX.toUpperCase());
 }
