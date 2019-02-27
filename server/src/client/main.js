ae9c3762e6a173b4e6791ee4abc6560e13e95c63
Roni Peled <roni@mediajel.com>
2019-02-26 19:04:44 -0500

    Increase speed

--- server/src/client/main.js
+++ server/src/client/main.js
@@ -272,14 +272,14 @@
                 if (fd.checked) {
                     if (face.x > 60) {
                         byteArray[0] = 3;
-                        byteArray[1] = 100;
+                        byteArray[1] = 150;
                         dataChannel.send(byteArray);
                         byteArray[0] = 2;
                         byteArray[1] = 4;
                         dataChannel.send(byteArray);
                     } else if (face.x < 40) {
                         byteArray[0] = 3;
-                        byteArray[1] = 100;
+                        byteArray[1] = 150;
                         dataChannel.send(byteArray);
                         byteArray[0] = 2;
                         byteArray[1] = 3;
